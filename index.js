b96a84e39ce8b252be1bd43745e748b46459d4d0
Your Name <you@example.com>
2024-07-16 22:31:51 +0800

    Your commit message

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const app = require("./src/app");
-const PORT=3000;
-app.listen(PORT,()=>{
-    console.log("Listenig on Port: "+PORT)
-})
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the public directory
+app.use(express.static('public'));
+
+// Route for the home page
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
--- src/app.js
+++
@@ -1,8 +0,0 @@
-const express= require('express');
-const app=express();
-app.use(express.json());
-app.use(express.urlencoded({extended:false}));
-const mainRoutes= require('./routes/mainRoutes')
-app.use("/api",mainRoutes);
-app.use("/", express.static('public'));
-module.exports=app;
